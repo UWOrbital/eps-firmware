ec8b958c92db378c85275222676ef7aa14386249
Urban Pistek <urban.pistek@gmail.com>
2022-03-01 20:56:03 -0500

    Updated structure, started adding timer

--- mppt-app/include/mppt.h
+++ mppt-app/include/mppt.h
@@ -1,26 +1,36 @@
 #include "mbed.h"
 #include "sensors.h"
 
+enum duty_cycle {DECREASE = 0, INCREASE = 1};
+
 class Mppt {
     
     private:
         float current, voltage;
-        float dv, di;  
+        float dv, di; 
+        int cycle_time_ms;
+        Timer timer;  
 
     public:
 
-        Mppt(float current, float voltage);
+        Mppt(float current, float voltage, int cycle_ms);
 
         void set_voltage(float voltage);
 
         void set_current(float current);
 
+        int calc_mppt();
+
         void run_mppt();
 
     private:
 
-        void calc_dv();
+        void calc_dv(){
+            
+        }
 
-        void calc_di();
+        void calc_di(){
+
+        }
 
 };
--- mppt-app/src/mppt.cpp
+++ mppt-app/src/mppt.cpp
@@ -1,18 +1,66 @@
 #include "../include/mppt.h"
 
-Mppt::Mppt(float current, float voltage){
-
+Mppt::Mppt(float current, float voltage, int cycle_ms){
+    current = current;
+    voltage = voltage;
+    cycle_time_ms = cycle_ms;
 }
 
 void Mppt::set_current(float current){
-
+    current = current;
 }
 
 void Mppt::set_voltage(float voltage){
-
+    voltage = voltage;
 }
 
 void Mppt::run_mppt(){
+
+    // Start timer to track time 
+    timer.start();
+     
+    while(true){
+
+    }
+}
+
+int Mppt::calc_mppt(){
      calc_di();
      calc_dv();
+
+     if (dv == 0){
+
+         if (di == 0){
+             return INCREASE;
+         } else {
+
+             if (di > 0){
+                 return INCREASE;
+             } else {
+                 return DECREASE;
+             }
+         }
+
+     } else {
+
+         float di_dv = di / dv; 
+         float i_v = current / voltage; 
+
+         if (di_dv == i_v){
+             return INCREASE;
+         } else {
+             
+             if (di_dv > -i_v){
+                 
+                 if (dv > 0){
+                     return INCREASE;
+                 } else {
+                     return DECREASE;
+                 }
+             } else {
+                 return DECREASE;
+             }
+         }
+
+     }
 }
