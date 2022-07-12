a4a5665807bb6785213e10c41772953273550fe6
Eugene Hong <eugeneh1217@gmail.com>
2022-07-11 22:31:32 -0400

    Fix compile errors

--- zephyr-apps/timer/src/main.c
+++ zephyr-apps/timer/src/main.c
@@ -19,7 +19,8 @@ static const struct gpio_dt_spec sw = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 void main(void)
 {
-	int ret, time;
+	int ret;
+	int time;
 	int rising_edge = 1;
 	LOG_INF("timer started\n");
 
@@ -42,7 +43,7 @@ void main(void)
 		else if (rising_edge == 1 && ret == 1)
 		{
 			rising_edge = 0;
-			time = k_uptime_get()
+			time = k_uptime_get();
 			LOG_INF("milliseconds since boot: %lld\n", time);
 		}
 	}
