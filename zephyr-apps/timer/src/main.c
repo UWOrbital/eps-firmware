3b9e352a09dc74747d8ef36e0275b2e2ca9c9906
Eugene Hong <eugeneh1217@gmail.com>
2022-07-12 12:33:43 -0400

    Update timer app

--- zephyr-apps/timer/src/main.c
+++ zephyr-apps/timer/src/main.c
@@ -1,19 +1,33 @@
-#include <zephyr/zephyr.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/kernel.h>
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#include <zephyr/zephyr.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(timer, LOG_LEVEL_INF);
+
+/* timer expiry function */
+void log_time(struct k_timer *timer_id)
+{
+	unsigned period, uptime;
+	period = k_ticks_to_ms_ceil32(timer_id->period.ticks);
+	uptime = k_uptime_get();
+	LOG_INF("timer(period=%d): %d", period, uptime);
+}
+
+/* timer definition(s) */
+K_TIMER_DEFINE(time_logging_4s_timer, log_time, NULL);
+K_TIMER_DEFINE(time_logging_2s_timer, log_time, NULL);
 
 void main(void)
 {
-	int time = 0; 
-	while (1) {
-		k_msleep(SLEEP_TIME_MS);
-		LOG_INF("timer...");
+	// announce app starts
+	LOG_INF("timer app started");
 
-		time = k_uptime_get();
-		LOG_INF("time %d", time);
-	}
+	// start timers
+	k_timer_start(&time_logging_4s_timer, K_SECONDS(1), K_SECONDS(4));
+	k_timer_start(&time_logging_2s_timer, K_SECONDS(1), K_SECONDS(2));
 }
